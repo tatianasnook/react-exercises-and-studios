5437d42abb6a2e00fbe3d5d4d2342906e5f84fd2
tatianasnook <tatsnook@gmail.com>
2024-08-21 13:08:21 -0500

    part1. React exercise

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,22 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book List";
+   let book1 = "https://m.media-amazon.com/images/I/41Vy1FFcz3L._SY445_SX342_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/5197S28VyHL._SY445_SX342_QL70_FMwebp_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/A1lnhRg0c4L._SX342_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <div>
+            <img src={book1} alt="Romantic Comedy by Curtis Sittenfield" width="250"/>
+         </div>
+         <div>
+            <img src={book2} alt="Tress of the Emerald Sea by Brandon Sanderson" width="250"/>
+         </div>
+         <div>
+            <img src={book3} alt="The London Seance Society by Sarah Penner" width="250"/>
+         </div>
+         
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,14 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import styles from './ChoresList.module.css';
+
+export default function ChoresList() {
+   return (
+      <div>
+         <h3 className={styles.choresHeading}>My chores list</h3>
+         <ul>
+            <li className={styles.choresText}>Task 1</li>
+            <li className={styles.choresText}>Task 2</li>
+            <li className={styles.choresText}>Task 3</li>
+         </ul>
+      </div>
+   )
 }
